b4a46a8fb8f81daca8d0ba591e6c783b976e8b6b
Miftakhus Salam <smiftakhus@gmail.com>
2021-06-10 13:44:24 +0700

    get started

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '739c3f83e1e22add8e2d163a96027608',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
